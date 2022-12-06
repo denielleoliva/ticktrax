064d798808f5cc03ab621c159ec53b617118de19
ryandahan2001 <dahanr@nevada.unr.edu>
2022-12-06 15:15:48 -0800

    change header sizing

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -3,29 +3,30 @@
         <h2>
             Create Account
         </h2>
-        <h7 style="margin:12px"> First Name</h7>
+        <h6 style="margin:6px; font-size:medium"> First Name</h6>
         <!-- First Name -->
         <q-input filled style="max-width: 500px; margin: 10px" 
         v-model="fName" type="name"/>
-        <h7 style="margin:12px"> Last Name</h7>
+        <h6 style="margin:6px; font-size:medium""> Last Name</h6>
         <!-- Last Name -->
         <q-input filled style="max-width: 500px; margin: 10px"
         v-model="lName" type="name"/>
-        <h7 style="margin:12px"> Email *</h7>
+        <h6 style="margin:6px; font-size:medium""> Email *</h6>
         <!-- Enter email -->
         <q-input filled style="max-width: 500px; margin: 10px;"
         v-model="email" type="email"  />
-        <h7 style="margin:12px"> Password *</h7>
+        <h6 style="margin:6px; font-size:medium""> Password *</h6>
         <!-- Enter Password -->
         <q-input filled style="max-width: 500px; margin: 10px;"
             v-model="password" :type="isPwd ? 'password' : 'text'"/>
         <!-- Confirm Password -->
-        <h7 style="margin:12px"> Confirm Password *</h7>
+        <h6 style="margin:6px; font-size:medium""> Confirm Password *</h6>
         <q-input filled style="max-width: 500px; margin: 10px;"
             v-model="cPassword" :type="isPwd ? 'password' : 'text'"/>
+        <h6 style="margin:6px; font-size:xx-small"> * indicates a required field </h6>
         <!-- Submit to DB -->
-        <q-btn style="margin-left:10px; margin-top:10px; background-color:skyblue"
-            @click="signIn(email, password)">
+        <q-btn style="margin-left:6px; margin-top:10px; background-color:skyblue"
+            @click="register(fName, lName, email, password, cPassword)">
             Sign in
         </q-btn>
     </q-page>
@@ -48,6 +49,9 @@ export default{
             password: ref(''),
             isPwd: ref(true),
             email: ref(''),
+            fName: ref(''),
+            lName: ref(''),
+            cPassword: ref('')
         }
     },
     methods:{
@@ -66,8 +70,15 @@ export default{
                 this.authFail = true,
                 console.log("authentication failed")
             }
-            
-        }   
+        }, 
+        register(fName, lName, email, password, cPassword)
+        {
+            const confirmCorrect = password === cPassword
+            if(confirmCorrect)
+            {
+                //send fName, lName, email, password to db
+            }
+        }
     }
 }
 </script>
