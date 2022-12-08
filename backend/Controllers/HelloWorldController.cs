cc3e173cd4cec135ecb83032e055c5001127389f
Denielle Oliva <denielle.oliva@gmail.com>
2022-12-07 21:20:38 -0800

    start of backend (template)

---
+++ backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class HelloWorldController : Controller
+    {
+        public HelloWorldController()
+        {}
+
+        [HttpGet]
+        public HelloWorld GetAll()
+        {
+            return new HelloWorld{ text = "Hello world from dotnet core api!"};
+        }
+    }
+}
