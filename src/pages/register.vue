11502176672eee6506a19a5430a2acec6169eae5
ryandahan2001 <dahanr@nevada.unr.edu>
2022-12-07 17:24:44 -0800

    add account registration success

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,38 +1,45 @@
 <template>
-    <q-page padding style="font-family: customfont; margin:10px">
+    <q-page padding style="font-family: customfont; margin:10px;">
         <h2>
             Create Account
         </h2>
-        <h6 style="margin:6px; font-size:medium"> First Name</h6>
+        <h6 style="margin:12px; font-size:medium"> First Name</h6>
         <!-- First Name -->
-        <q-input filled style="max-width: 500px; margin: 10px" 
+        <q-input filled style="max-width: 500px; margin: 10px;"
         v-model="fName" type="name"/>
-        <h6 style="margin:6px; font-size:medium""> Last Name</h6>
+        <h6 style="margin:12px; font-size:medium"> Last Name</h6>
         <!-- Last Name -->
         <q-input filled style="max-width: 500px; margin: 10px"
         v-model="lName" type="name"/>
-        <h6 style="margin:6px; font-size:medium""> Email *</h6>
+        <h6 style="margin:12px; font-size:medium"> Email *</h6>
         <!-- Enter email -->
         <q-input filled style="max-width: 500px; margin: 10px;"
         v-model="email" type="email"  />
-        <h6 style="margin:6px; font-size:medium""> Password *</h6>
+        <h6 style="margin:12px; font-size:medium"> Password *</h6>
         <!-- Enter Password -->
         <q-input filled style="max-width: 500px; margin: 10px;"
             v-model="password" :type="isPwd ? 'password' : 'text'"/>
         <!-- Confirm Password -->
-        <h6 style="margin:6px; font-size:medium""> Confirm Password *</h6>
-        <q-input filled style="max-width: 500px; margin: 10px;"
+        <h6 style="margin:12px; font-size:medium"> Confirm Password *</h6>
+        <q-input filled style="max-width: 500px; margin: 10px;" 
             v-model="cPassword" :type="isPwd ? 'password' : 'text'"/>
-        <h6 style="margin:6px; font-size:xx-small"> * indicates a required field </h6>
+        <div class="row">
+            <q-btn style="align:center; margin-left:12px; margin-top:10px; background-color:skyblue"
+                @click="register(fName, lName, email, password, cPassword)">
+                Sign in
+            </q-btn>
+            <div class="col"  v-if="(registerSuccess === true)" style="align-content: center; padding:12px; font-size:large">
+                Account Creation Successful!
+            </div>
+            <div class="col" v-if="(registerSuccess === false)" style="align-content: center; padding:12px; font-size:large"> 
+                * indicates a required field 
+            </div>
+        </div>
         <!-- Submit to DB -->
-        <q-btn style="margin-left:6px; margin-top:10px; background-color:skyblue"
-            @click="register(fName, lName, email, password, cPassword)">
-            Sign in
-        </q-btn>
     </q-page>
   </template>
   
-  <script>
+<script>
 import { ref } from 'vue'
 
 export default{
@@ -41,6 +48,7 @@ export default{
             leftDrawer: false,
             signedIn: false,
             authFail: false,
+            registerSuccess: false,
         }
     },
     setup () 
@@ -55,30 +63,34 @@ export default{
         }
     },
     methods:{
-        signIn(email, password) 
-        {
-            const authenticated = email === "jdoe@fakemail.com" && password == "fakePassword123!"
-            if(authenticated)
+            signIn(email, password) 
             {
-                this.signedIn = true,
-                this.authFail = false,
-                this.$router.push('/profile/1/')
-                console.log("signedIn =" + this.signedIn + " authFail =" + this.authFail)
-            }
-            else
+                const authenticated = email === "jdoe@fakemail.com" && password == "fakePassword123!"
+                if(authenticated)
+                {
+                    this.signedIn = true,
+                    this.authFail = false,
+                    this.$router.push('/profile/1/')
+                    console.log("signedIn =" + this.signedIn + " authFail =" + this.authFail)
+                }
+                else
+                {
+                    this.authFail = true,
+                    console.log("authentication failed")
+                }
+            }, 
+            register(fName, lName, email, password, cPassword)
             {
-                this.authFail = true,
-                console.log("authentication failed")
-            }
-        }, 
-        register(fName, lName, email, password, cPassword)
-        {
-            const confirmCorrect = password === cPassword
-            if(confirmCorrect)
-            {
-                //send fName, lName, email, password to db
-            }
+                const validRegister = password === cPassword && password !== "" && cPassword !== "" && email !== ""
+                if(validRegister)
+                {
+                    console.log(fName + " " + lName + " " + email + " " + password + " ")
+                    //send fName, lName, email, password to db
+                    this.registerSuccess = true
+                }
+                else
+                    console.log(validRegister)
+            },
         }
     }
-}
 </script>
