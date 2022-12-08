c2574cdf9d884eff3dbcb2b91c1a93c6b44f3845
Denielle Oliva <denielle.oliva@gmail.com>
2022-12-07 22:14:35 -0800

    logo addition and profile page touch ups

--- src/layouts/landing.vue
+++ src/layouts/landing.vue
@@ -2,14 +2,14 @@
   <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
 
     <!-- (Optional) The Header -->
-    <q-header elevated class="row" style="background-color:skyblue; color:black;">
+    <q-header elevated class="row" style="background-color:#5CAB7D; color:white;">
       <q-toolbar class="col-auto">
         <q-toolbar-title style="font-family:customfont"> 
           ticktrax
         </q-toolbar-title>
       </q-toolbar>
 
-      <q-tabs style="background-color: skyblue">
+      <q-tabs style="background-color: #5CAB7D">
         <q-route-tab 
           to="/"
           replace
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,9 @@
 <template>
   <q-page class="flex flex-center">
-    <h1 style="font-family: customfont">Welcome to ticktrax</h1>
+    <h1 style="font-family: customfont; padding:20px;">Welcome to ticktrax</h1>
+    <q-img style="float: left; padding: 30px 30px 30px 30px;" src="../assets/ticktrax.png" width="80px">
+
+    </q-img>
   </q-page>
 </template>
 
