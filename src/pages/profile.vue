35dfb683a270df0d8e4dabade3124021ea2954bd
Sultan7rs <sultan7rs@gmail.com>
2023-02-11 22:19:46 -0800

    profile page redesign

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,129 +1,78 @@
 <template>
-  <q-page class="q-pa-sm">
+  <q-page class="q-pa-lg q-mx-auto q-my-xl" style="max-width: 1000px">
+    <div class="gt-sm">
+    <q-splitter
+      v-model="splitterModel"
+    >
 
-    <div class="row q-col-gutter-sm justify-center">
+      <template v-slot:before>
+        <q-tabs
+          v-model="tab"
+          vertical
+          class="text-grey"
+          active-color="positive"
+          indicator-color="positive"
+        >
+          <q-tab name="general"  label="General" />
+          <q-tab name="profile"  label="Edit Profile" />
+          <q-tab name="password"  label="Password" />
+        </q-tabs>
 
-      <div class="col-lg-8 col-md-8 col-xs-12 col-sm-12">
-        
-        <q-card class="card-bg text-white">
-          <q-card-section class="text-h6 ">
-            <div class="text-h6">Edit Profile</div>
-            <div class="text-subtitle2">Complete your profile</div>
-          </q-card-section>
-          <q-card-section class="q-pa-sm">
-            <q-list class="row">
-              <q-item class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <q-item-section side>
-                  <q-avatar size="100px">
-                    <img src="../assets/blank_avatar.png">
-                  </q-avatar>
-                </q-item-section>
-                <q-item-section>
-                  <q-btn label="Add Photo" class="text-capitalize" rounded color="info"
-                         style="max-width: 120px"></q-btn>
-                </q-item-section>
-              </q-item>
+      </template>
 
-              <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <q-item-section>
-                  <q-input dark color="white" dense v-model="user_details.user_name" label="User Name"/>
-                </q-item-section>
-              </q-item>
-              <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <q-item-section>
-                  <q-input dark color="white" dense v-model="user_details.email" label="Email Address"/>
-                </q-item-section>
-              </q-item>
-              <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <q-item-section>
-                  <q-input dark color="white" dense v-model="user_details.first_name" label="First Name"/>
-                </q-item-section>
-              </q-item>
-              <q-item class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <q-item-section>
-                  <q-input dark color="white" dense v-model="user_details.last_name" label="Last Name"/>
-                </q-item-section>
-              </q-item>
-              <q-item class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <q-item-section>
-                  <q-input dark color="white" type="textarea" dense v-model="user_details.about" label="About"/>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-card-section>
-          <q-card-actions align="right">
-            <q-btn class="text-capitalize bg-info text-white">Update User Info</q-btn>
-          </q-card-actions>
-        </q-card>
-      </div>
+      <template v-slot:after>
+        <profile-panel :tab="tab"  />
+      </template>
 
-      <div class="col-lg-8 col-md-8 col-xs-12 col-sm-12">
-        <q-card class="card-bg text-white">
-          <q-card-section class="text-h6 q-pa-sm">
-            <div class="text-h6">Change Password</div>
-          </q-card-section>
-          <q-card-section class="q-pa-sm row">
-            <q-item class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <q-item-section>
-                Current Password
-              </q-item-section>
-            </q-item>
-            <q-item class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-              <q-item-section>
-                <q-input type="password" dark dense outlined color="white" round
-                         v-model="password_dict.current_password"
-                         label="Current Password"/>
-              </q-item-section>
-            </q-item>
-            <q-item class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <q-item-section>
-                New Password
-              </q-item-section>
-            </q-item>
-            <q-item class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-              <q-item-section>
-                <q-input type="password" dark dense outlined color="white" round v-model="password_dict.new_password"
-                         label="New Password"/>
-              </q-item-section>
-            </q-item>
-            <q-item class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <q-item-section>
-                Confirm New Password
-              </q-item-section>
-            </q-item>
-            <q-item class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
-              <q-item-section>
-                <q-input type="password" dark dense outlined round color="white"
-                         v-model="password_dict.confirm_new_password"
-                         label="Confirm New Password"/>
-              </q-item-section>
-            </q-item>
-          </q-card-section>
-          <q-card-actions align="right">
-            <q-btn class="text-capitalize bg-info text-white">Change Password</q-btn>
-          </q-card-actions>
-
-        </q-card>
-      </div>
+    </q-splitter>
+    </div>
+    <div class="lt-md">
+    <q-tabs
+      v-model="tab"
+      dense
+      class="text-grey q-pa-none"
+      active-color="positive"
+      indicator-color="positive"
+      align="center"
+      narrow-indicator
+    >
+      <q-tab name="general"  label="General" />
+      <q-tab name="profile"  label="Edit Profile" />
+      <q-tab name="password"  label="Password" />
+    </q-tabs>
+    <q-separator />
+    <profile-panel :tab="tab"  />
     </div>
   </q-page>
 </template>
 
 <script>
-import {defineComponent} from 'vue'
+const image = ref(null);
+import { defineComponent, ref } from 'vue';
+import ProfilePanel from "components/ProfilePanel.vue";
 export default defineComponent({
-  name: "UserProfile",
+  name: 'UserProfile',
+  components: {ProfilePanel},
   setup() {
+    const image = ref(null);
+    const imageUrl = ref('');
     return {
       user_details: {},
-      password_dict: {}
-    }
-  }
-})
+      password_dict: {},
+      image,
+      ProfilePanel,
+      imageUrl,
+      tab: ref('general'),
+      splitterModel: ref(20),
+      updateFile() {
+        imageUrl.value = URL.createObjectURL(image.value);
+      },
+    };
+  },
+});
 </script>
 
-<style scoped>
-.card-bg {
-  background-color: #5CAB7D;
-}
+<style scoped lang="sass">
+.navLinks:hover
+  background-color: rgb(33, 186, 69, 0.4)
 </style>
