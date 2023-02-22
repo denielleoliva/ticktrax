6c6d7e89f0b682df375bcf922da258363a0f9e03
ryandahan2001 <dahanr@nevada.unr.edu>
2023-02-21 16:43:07 -0800

    delete profile button merge conflict resolve

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,54 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1 style="font-family: customfont; padding:20px;">Welcome to ticktrax</h1>
-    <q-img style="float: left; padding: 30px 30px 30px 30px;" src="../assets/ticktrax.png" width="80px">
-
-    </q-img>
+  <q-page align="center">
+    <div class="translateUp" >
+      <h1 style="font-family:customfont; font-size:xx-large">
+        Welcome to ticktrax
+        <q-img src="../assets/ticktrax.png" width="50px"/>
+      </h1>
+    </div>
+    <!-- <div class="fadeIn" >
+      <q-btn color="primary">
+        sign in
+      </q-btn>
+      <q-btn color="primary" >
+        sign up
+      </q-btn>
+    </div> -->
+    <tick-list class="fadeIn"/>
   </q-page>
 </template>
 
 <script>
+import TickList from 'components/TickList.vue'
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  components: {
+    TickList
+  }
 }
 </script>
+
+<style>
+
+.translateUp{
+  position: relative;
+  animation-name: translate;
+  animation-duration: 1s;
+  animation-direction: reverse;
+}
+
+.fadeIn{
+  animation-name: fadeIn;
+  animation-duration: 2s;
+}
+
+@keyframes translate {
+  100% {  transform: translateY(200%) }
+}
+
+@keyframes fadeIn {
+  0% {  opacity:0;  }
+  100% {  opacity:1;  }
+}
+</style>
